c022d63e5259edd4a59e092b1311d5a778fcd935
P-mm02 <poomtawee.mm02@gmail.com>
2023-10-25 00:57:44 +0700

    first commit

---
+++ home.js
@@ -0,0 +1,7 @@
+const navbar = document.querySelector('.navbar');
+const secondBar = document.querySelector('.secondBar');
+const topBackground = document.querySelector('.topBackground');
+topBackground.style.height = navbar.clientHeight + 'px';
+const searchImg = document.querySelector('.searchImg');
+const secondBarRect = secondBar.getBoundingClientRect();
+searchImg.style.marginTop = secondBarRect.height -40 + 'px';
